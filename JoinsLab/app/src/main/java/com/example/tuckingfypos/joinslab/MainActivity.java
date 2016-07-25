0eb783b661479e4033f517c2ff6e929819c6be96
James DeLayo <james.delayo@gmail.com>
2016-07-24 20:30:52 -0400

    fixes single-point data addition

--- JoinsLab/app/src/main/java/com/example/tuckingfypos/joinslab/MainActivity.java
+++ JoinsLab/app/src/main/java/com/example/tuckingfypos/joinslab/MainActivity.java
@@ -59,18 +59,22 @@ public class MainActivity extends AppCompatActivity {
             public void onClick(View view) {
                 final DatabaseHelper helper = DatabaseHelper.getInstance(MainActivity.this);
                 Cursor cursor = helper.getMacysEmployees();
-                CursorAdapter adapter = new CursorAdapter(MainActivity.this,cursor, CursorAdapter.FLAG_REGISTER_CONTENT_OBSERVER){
+                CursorAdapter adapter = new CursorAdapter(MainActivity.this,cursor, CursorAdapter.
+                        FLAG_REGISTER_CONTENT_OBSERVER){
 
                     @Override
                     public View newView(Context context, Cursor cursor, ViewGroup parent) {
-                        return LayoutInflater.from(context).inflate(android.R.layout.simple_list_item_1,parent,false);
+                        return LayoutInflater.from(context).
+                                inflate(android.R.layout.simple_list_item_1,parent,false);
                     }
 
                     @Override
                     public void bindView(View view, Context context, Cursor cursor) {
                         TextView textView = (TextView) view.findViewById(android.R.id.text1);
-                        String firstName = cursor.getString(cursor.getColumnIndex(DatabaseHelper.COL_FIRST_NAME));
-                        String lastName = cursor.getString(cursor.getColumnIndex(DatabaseHelper.COL_LAST_NAME));
+                        String firstName = cursor.
+                                getString(cursor.getColumnIndex(DatabaseHelper.COL_FIRST_NAME));
+                        String lastName = cursor.
+                                getString(cursor.getColumnIndex(DatabaseHelper.COL_LAST_NAME));
                         String fullName = firstName + " "+lastName + " ";
                         textView.setText(fullName);
                     }
@@ -85,17 +89,20 @@ public class MainActivity extends AppCompatActivity {
             public void onClick(View view) {
                 final DatabaseHelper helper = DatabaseHelper.getInstance(MainActivity.this);
                 Cursor cursor = helper.getBostonCompanies();
-                CursorAdapter adapter = new CursorAdapter(MainActivity.this,cursor,CursorAdapter.FLAG_REGISTER_CONTENT_OBSERVER){
+                CursorAdapter adapter = new CursorAdapter(MainActivity.this,cursor,CursorAdapter.
+                        FLAG_REGISTER_CONTENT_OBSERVER){
 
                     @Override
                     public View newView(Context context, Cursor cursor, ViewGroup parent) {
-                        return LayoutInflater.from(context).inflate(android.R.layout.simple_list_item_1,parent,false);
+                        return LayoutInflater.from(context).
+                                inflate(android.R.layout.simple_list_item_1,parent,false);
                     }
 
                     @Override
                     public void bindView(View view, Context context, Cursor cursor) {
                         TextView textView = (TextView) view.findViewById(android.R.id.text1);
-                        textView.setText(cursor.getString(cursor.getColumnIndex(helper.COL_COMPANY)));
+                        textView.setText(cursor.getString(cursor.
+                                getColumnIndex(helper.COL_COMPANY)));
                     }
                 };
 
@@ -122,11 +129,11 @@ public class MainActivity extends AppCompatActivity {
             public void onClick(View view) {
                 AlertDialog.Builder builder = new AlertDialog.Builder(context);
                 LayoutInflater inflater = LayoutInflater.from(context);
-                final View dialogLayout = inflater.inflate(R.layout.alert_dialog, null);
+                final View dialogLayout = inflater.inflate(R.layout.input_dialogue, null);
                 builder.setView(dialogLayout);
 
 
-                builder.setTitle("Tables-lab");
+                builder.setTitle("JoinsLab");
 
                 builder.setNegativeButton("CANCEL", new DialogInterface.OnClickListener() {
                     @Override
@@ -144,7 +151,8 @@ public class MainActivity extends AppCompatActivity {
                 dialog.show();
 
 
-                dialog.getButton(DialogInterface.BUTTON_POSITIVE).setOnClickListener(new View.OnClickListener() {
+                dialog.getButton(DialogInterface.BUTTON_POSITIVE).
+                        setOnClickListener(new View.OnClickListener() {
                     @Override
                     public void onClick(View view) {
                         Toast.makeText(MainActivity.this, "DATA ADDED", Toast.LENGTH_SHORT).show();
@@ -154,15 +162,15 @@ public class MainActivity extends AppCompatActivity {
                         EditText edittext3 = (EditText) dialogLayout.findViewById(R.id.lastNameEditText);
                         EditText edittext4 = (EditText) dialogLayout.findViewById(R.id.dobEditText);
                         EditText edittext5 = (EditText) dialogLayout.findViewById(R.id.cityEditText);
-                        if (edittext1.getText().toString().length() == 0) {
+                        if (edittext1.getText().toString().length() < 1) {
                             edittext1.setError("Please type in your SSN");
-                        } else if (edittext2.getText().toString().length() == 0) {
+                        } else if (edittext2.getText().toString().length() < 1) {
                             edittext2.setError("Please type in your first name");
-                        } else if (edittext3.getText().toString().length() == 0) {
+                        } else if (edittext3.getText().toString().length() < 1) {
                             edittext3.setError("Please type in your last name");
-                        } else if (edittext4.getText().toString().length() == 0) {
+                        } else if (edittext4.getText().toString().length() < 1) {
                             edittext4.setError("Please type in your year of birth");
-                        } else if (edittext5.getText().toString().length() == 0) {
+                        } else if (edittext5.getText().toString().length() < 1) {
                             edittext5.setError("Please type in your city");
                         } else {
                             employee.setmSSN((edittext1.getText().toString()));
@@ -175,13 +183,10 @@ public class MainActivity extends AppCompatActivity {
                     }
                 });
 
-                dialog.getButton(DialogInterface.BUTTON_NEGATIVE).setOnClickListener(new View.OnClickListener() {
+                dialog.getButton(DialogInterface.BUTTON_NEGATIVE).
+                        setOnClickListener(new View.OnClickListener() {
                     @Override
-
-                    public void onClick(View view) {
-                        Toast.makeText(MainActivity.this, "works!", Toast.LENGTH_SHORT).show();
-                        dialog.dismiss();
-                    }
+                    public void onClick(View view) {dialog.dismiss();}
                 });
 
 
